6e8022ca607688c57db87d026c39c836ccc54e4f
Jasper de Wilde <j.de.wilde1997@gmail.com>
2018-11-13 20:37:15 +0100

    Added lcd

--- Arduino/BTUltraSonic/BTUltraSonic.ino
+++ Arduino/BTUltraSonic/BTUltraSonic.ino
@@ -1,3 +1,10 @@
+//  Librabies
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+//  Set the LCD address to 0x3F for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
 //  Definitions.
 #define trigPin 3
 #define echoPin 4
@@ -7,6 +14,7 @@
 long duration;
 int distance;
 String timestamp;
+int customers = 0;
 
 //  Setup code.
 void setup() {
@@ -16,6 +24,9 @@ void setup() {
   digitalWrite(ledPin, LOW);
   digitalWrite(trigPin, LOW);
   Serial.begin(38400);
+
+  lcd.begin();
+  lcd.backlight();
 
   timestamp = "enter";
 }
@@ -39,8 +50,14 @@ void loop() {
   if (distance < 100) {
     //  Turn on LED.
     digitalWrite(ledPin, HIGH);
+    customers++;
     
     Serial.println(timestamp);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Customers: ");
+    lcd.setCursor(11, 0);
+    lcd.print(customers);
     delay(500);
   } else {
     //  If no object detected, turn off LED and wait.
