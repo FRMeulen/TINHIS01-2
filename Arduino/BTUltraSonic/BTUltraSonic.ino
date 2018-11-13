207d934dff460407eb9bbca41a9cd0fa007e4885
Jasper de Wilde <j.de.wilde1997@gmail.com>
2018-11-13 22:52:32 +0100

    Added second ultrasonic sensor

--- Arduino/BTUltraSonic/BTUltraSonic.ino
+++ Arduino/BTUltraSonic/BTUltraSonic.ino
@@ -6,23 +6,26 @@
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 //  Definitions.
-#define trigPin 3
-#define echoPin 4
+#define trigPinOne 3
+#define echoPinOne 4
+#define pingPin 6
 #define ledPin 2
 
 //  Variables.
-long duration;
-int distance;
+long durationOne;
+int distanceOne;
+long durationTwo;
+int distanceTwo;
 String timestamp;
 int customers = 0;
 
 //  Setup code.
 void setup() {
   pinMode(ledPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(trigPin, OUTPUT);
+  pinMode(echoPinOne, INPUT);
+  pinMode(trigPinOne, OUTPUT);
   digitalWrite(ledPin, LOW);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPinOne, LOW);
   Serial.begin(38400);
 
   lcd.begin();
@@ -34,25 +37,51 @@ void setup() {
 //  Loop code.
 void loop() {
   //  Scan for objects.
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPinOne, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPinOne, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPinOne, LOW);
 
   //  Retrieve pulse duration.
-  duration = pulseIn(echoPin, HIGH);
+  durationOne = pulseIn(echoPinOne, HIGH);
+
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin, LOW);
+
+  pinMode(pingPin, INPUT);
+  durationTwo = pulseIn(pingPin, HIGH);
 
   //  Calculate distance to object.
-  distance = duration*0.034/2;
+  distanceOne = durationOne*0.034/2;
+  distanceTwo = durationTwo*0.034/2;
 
   //  Act on results.
-  if (distance < 100) {
+  if (distanceOne < 100) {
     //  Turn on LED.
     digitalWrite(ledPin, HIGH);
     customers++;
     
     Serial.println(timestamp);
+    Serial.println("distanceOne");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Customers: ");
+    lcd.setCursor(11, 0);
+    lcd.print(customers);
+    delay(500);
+  } else if (distanceTwo < 100) {
+    //  Turn on LED.
+    digitalWrite(ledPin, HIGH);
+    customers++;
+    
+    Serial.println(timestamp);
+    Serial.println("distanceTwo");
+    Serial.println(distanceTwo);
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Customers: ");
